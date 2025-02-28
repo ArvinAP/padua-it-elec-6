9e7eccd4dbfec2aac487cd80771ffd145cb15370
pads <padua.arvin@gmail.com>
2025-02-28 14:04:07 +0800

    added backend feature to show posts in console

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,31 @@
 
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', "*");
     res.setHeader("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
 
-    next();
-})
+    res.setHeader('Access-Control-Allow-Methods',
+        "GET, POST, PATCH, DELETE, OPTIONS")
+    next();}
+    )
 
-app.use('/api/posts',(req, res, next) => {
+
+    app.post('/api/posts', (req, res, next) => {
+        const post = req.body;
+        console.log(post);
+        res.status(201).json({
+            message: "Post added successfully"
+        });
+    })
+
+app.get('/api/posts',(req, res, next) => {
     const posts = 
         [{
             id: "adad",
